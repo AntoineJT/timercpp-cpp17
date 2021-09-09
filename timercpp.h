a46740c74ddaa72dc845fb3c616db394a5ecd25e
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-09 03:35:13 +0200

    Restore parameters original names

--- timercpp.h
+++ timercpp.h
@@ -8,30 +8,30 @@ class Timer {
 	std::atomic<bool> active{true};
 	
     public:
-        void setTimeout(std::function<void()> callback, int delay);
-        void setInterval(std::function<void()> callback, int interval);
+        void setTimeout(std::function<void()> function, int delay);
+        void setInterval(std::function<void()> function, int interval);
         void stop();
 
 };
 
-void Timer::setTimeout(std::function<void()> callback, int delay) {
+void Timer::setTimeout(std::function<void()> function, int delay) {
     active = true;
     std::thread t([=]() {
         if(!active.load()) return;
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
         if(!active.load()) return;
-        callback();
+        function();
     });
     t.detach();
 }
 
-void Timer::setInterval(std::function<void()> callback, int interval) {
+void Timer::setInterval(std::function<void()> function, int interval) {
     active = true;
     std::thread t([=]() {
         while(active.load()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(interval));
             if(!active.load()) return;
-            callback();
+            function();
         }
     });
     t.detach();
