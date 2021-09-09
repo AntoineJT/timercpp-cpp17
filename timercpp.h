cfb799d59b187bc82c2e3437dd20d40061c8390c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2021-09-09 03:26:43 +0200

    Make it compatible with C++17

--- timercpp.h
+++ timercpp.h
@@ -2,35 +2,36 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <functional>
 
 class Timer {
 	std::atomic<bool> active{true};
 	
     public:
-        void setTimeout(auto function, int delay);
-        void setInterval(auto function, int interval);
+        void setTimeout(std::function<void()> callback, int delay);
+        void setInterval(std::function<void()> callback, int interval);
         void stop();
 
 };
 
-void Timer::setTimeout(auto function, int delay) {
+void Timer::setTimeout(std::function<void()> callback, int delay) {
     active = true;
     std::thread t([=]() {
         if(!active.load()) return;
         std::this_thread::sleep_for(std::chrono::milliseconds(delay));
         if(!active.load()) return;
-        function();
+        callback();
     });
     t.detach();
 }
 
-void Timer::setInterval(auto function, int interval) {
+void Timer::setInterval(std::function<void()> callback, int interval) {
     active = true;
     std::thread t([=]() {
         while(active.load()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(interval));
             if(!active.load()) return;
-            function();
+            callback();
         }
     });
     t.detach();
